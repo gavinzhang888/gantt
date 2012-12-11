66adbada97c12c4e89c8aab4856c549ed6d4b762
Titan Tang <titantang@gmail.com>
2012-12-11 12:47:43 +0800

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gantt::Application.config.secret_token = '35f580af2ef9e47fb94ba0a5e1212a04531219b60b7137c14ad5dab1d70b287fa7dbf11401bbe356f41de4c088a5c07db1de48ab61e5953db5002617a7bb17f4'
